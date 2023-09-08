1cc82aecd08a7019bd79b2daac39872e25a228c4
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-08 10:44:00 -0600

    feat: add background styling and removed base css

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { ref, onMounted, computed, watch } from "vue";
 // v-model = implement a two-way binding for input elements??
 
 const tasks = ref([]);
-const name = ref('')
+const name = ref("");
 const userTitle = ref("");
 const userCategory = ref(null);
 
@@ -19,28 +19,36 @@ const tasksOrder = computed(() =>
 // ascending list by created time??
 
 watch(name, (newNameValue) => {
-  localStorage.setItem('name', newNameValue)
-})
+  localStorage.setItem("name", newNameValue);
+});
 
 onMounted(() => {
-  name.value = localStorage.getItem('name') || ''
-})
+  name.value = localStorage.getItem("name") || "";
+});
 // set it so it stays on refresh??
-
 </script>
 
 <template>
+  <header>
+    <h1 class='website-title'>TaskMaster</h1>
+  </header>
   <main class="app">
     <section class="greeting">
       <h2 class="title">
-        Hello <input type='text' placeholder="Enter name here" v-model='name'/>
-        
+        Hello <input type="text" placeholder="Enter name here" v-model="name" />
       </h2>
+    </section>
+
+    <section>
+      <h3>Create a New Task</h3>
     </section>
   </main>
 </template>
 
 <style scoped>
+header {
+  text-align: center;
+}
 /* input:not([type="radio"]):not([type="checkbox"]), button {
 	appearance: none;
 	border: none;
