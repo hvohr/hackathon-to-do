b15408cb3e9f3d874bb402d14ac4febf6241cd82
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-08 14:29:50 -0600

    feat: add styling to all aspects

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ const userCategory = ref(null);
 
 const tasksOrder = computed(() =>
   tasks.value.sort((a, b) => {
-    return a.createdAt - b.createdAt;
+    return b.createdAt - a.createdAt;
   })
 );
 // ascending list by created time??
@@ -48,17 +48,10 @@ watch(
   { deep: true }
 );
 
-console.log(userCategory.value);
-
 // deep looks through each induvidual array item/element
 // and if anything changes, deep will catch it)
 
-watch(name, (newNameValue) => {
-  localStorage.setItem("name", newNameValue);
-});
-
 onMounted(() => {
-  name.value = localStorage.getItem("name") || "";
   tasks.value = JSON.parse(localStorage.getItem("tasks")) || [];
 });
 // set it so it stays on refresh??
@@ -69,13 +62,19 @@ onMounted(() => {
     <h1 class="website-title">TaskMaster</h1>
   </header>
   <main class="app">
-    <section class="greeting">
+    <!-- <section class="greeting">
       <h2 class="title">
-        Hello <input type="text" placeholder="Enter name here" v-model="name" />
+        Hello
+        <input
+          class="user-input-name"
+          type="text"
+          placeholder="Enter name here"
+          v-model="name"
+        />
       </h2>
-    </section>
+    </section> -->
 
-    <section>
+    <section class="create-container">
       <h3>Create a New Task</h3>
       <form @submit.prevent="addTask">
         <h4>What's on your task list?</h4>
@@ -94,7 +93,7 @@ onMounted(() => {
               v-model="userCategory"
             />
             <div class="bubble easy"></div>
-            <div>Easy</div>
+            <div class="easy">easy</div>
           </label>
           <label>
             <input
@@ -104,7 +103,7 @@ onMounted(() => {
               v-model="userCategory"
             />
             <div class="bubble medium"></div>
-            <div>Medium</div>
+            <div class="medium">medium</div>
           </label>
           <label>
             <input
@@ -114,24 +113,25 @@ onMounted(() => {
               v-model="userCategory"
             />
             <div class="bubble hard"></div>
-            <div>Hard</div>
+            <div class="hard">hard</div>
           </label>
         </div>
         <input type="submit" value="Add Task" />
       </form>
     </section>
-    <section>
+    <section class="task-list-container">
       <h3>Task List</h3>
+      <h5 v-if="tasks.length === 0">No Tasks Yet -- Add Some Above !!</h5>
       <div class="list">
         <div
           v-for="task in tasksOrder"
           :key="task.id"
           :class="`task-item ${task.done && 'done'}`"
         >
-          <label>
+          <label class="list-label">
             <input type="checkbox" v-model="task.done" />
-            <p :class="`bubble ${task.category}`"></p>
-            {{ task.category }}
+            <div :class="`${task.category}`"></div>
+            <div :class="`${task.category}`">{{ task.category }}</div>
           </label>
           <div class="content">
             <input type="text" v-model="task.content" />
@@ -149,6 +149,100 @@ onMounted(() => {
 header {
   text-align: center;
 }
+.create-container {
+  background-color: rgba(255, 255, 255, 0.5);
+  width: 80%;
+  box-shadow: 8px 4px 4px grey;
+  display: flex;
+  margin-top: 20px;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 20px;
+  text-align: center;
+  padding: 10px;
+}
+label {
+  margin: 20px;
+}
+
+.title {
+  background: none;
+  border: none;
+  font-size: 25px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+}
+
+h3 {
+  font-size: 25px;
+  text-align: center;
+  text-decoration: underline;
+}
+
+input[type="text"] {
+  padding: 20px;
+  font-size: 15px;
+}
+
+input[type="checkbox"] {
+  height: 20px;
+  width: 20px;
+  outline: 2px solid rgb(0, 0, 0);
+  margin-right: 15px;
+}
+
+input[type="submit"],
+.delete {
+  font-size: 20px;
+  background-color: #ffffff;
+  border: double 3px black;
+}
+
+.list-label {
+  display: flex;
+}
+
+.task-item {
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+  justify-content: space-around;
+  border: double 3px black;
+  width: 1000px;
+  padding: 20px;
+  background-color: rgba(255, 255, 255, 0.559);
+  margin: 10px;
+}
+
+.easy {
+  color: green;
+  font-size: 20px;
+}
+.medium {
+  color: rgb(216, 109, 15);
+  font-size: 20px;
+}
+.hard {
+  color: rgb(216, 35, 19);
+  font-size: 20px;
+}
+.difficulty-options {
+  display: flex;
+  justify-content: center;
+}
+
+h1 {
+  margin-bottom: 0px;
+  font-size: 50px;
+  border-bottom: 2px solid black;
+}
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 input[type="radio"] {
   appearance: none;
   padding: 10px;
@@ -161,7 +255,19 @@ input[type="radio"] {
 input[type="radio"]:checked {
   background-color: rgb(12, 67, 12);
 }
-.bubble.easy {
-  background-color: rgb(242, 89, 1);
+
+h5 {
+  font-size: 20px;
+  font-style:oblique;
+  color:rgb(34, 30, 33)
+}
+
+.user-input-name {
+  background: none;
+  border: none;
+  font-size: 25px;
+  text-align: center;
+  width: auto;
+  min-width: 1em;
 }
 </style>
